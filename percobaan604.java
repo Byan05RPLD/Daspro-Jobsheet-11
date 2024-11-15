d4bd68e41436d36cf743677b30c5bdfddb0e400d
Anandya Almas <byanmalang123@gmail.com>
2024-11-15 09:29:20 +0700

    menggunakan fungsi

--- percobaan604.java
+++ percobaan604.java
@@ -1,25 +1,27 @@
 import java.util.Scanner;
 public class percobaan604 {
   
+    static int hitungLuas(int panjang, int lebar) {
+        return panjang * lebar;
+    }
+    static int hitungVolume(int tinggi, int panjang, int lebar) {
+        int luas = hitungLuas(panjang, lebar);
+        return luas * tinggi;
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        // Menghitung luas persegi panjang
         System.out.print("Masukkan panjang: ");
         int p = input.nextInt();
         System.out.print("Masukkan lebar: ");
         int l = input.nextInt();
-        int luas = p * l;
-        System.out.println("Luas persegi panjang adalah: " + luas);
-
-        // Menghitung volume balok
         System.out.print("Masukkan tinggi: ");
         int t = input.nextInt();
-        int volume = luas * t;
+        int luas = hitungLuas(p, l);
+        int volume = hitungVolume(t, p, l);
+        System.out.println("Luas persegi panjang adalah: " + luas);
         System.out.println("Volume balok adalah: " + volume);
 
         input.close();
     }
 }
-
 
